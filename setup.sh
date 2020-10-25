1eaf7cc7e82f0c4ed442acef556efecb53e8ae72
abduRahman94 <sabdouseye@gmail.com>
2020-10-25 15:24:37 +0000

    refactor: modified app.py and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,2 @@
-:
-
-
+export DATABASE_URL = "postgres://mqmdblhhxisopk:f0b2d8a5be4879dca6ae1bcae98373e8df82f85b6201cfa68b05ca2316e404df@ec2-52-21-247-176.compute-1.amazonaws.com:5432/dti19cv28k6mu"
+export EXCITED = true
